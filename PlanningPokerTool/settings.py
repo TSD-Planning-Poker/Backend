65e04a3e0f833a3e4567d4a4ec6700548e2f06b9
Tesfamichael1074 <hope.miky1074@gmail.com>
2022-04-24 00:29:03 +0200

    Remove a hardcoded Django secret key

--- PlanningPokerTool/settings.py
+++ PlanningPokerTool/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
