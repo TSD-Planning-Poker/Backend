78fce4ad4204a7b07e0f8c1a1da06c3853d91fd8
krzk426 <krzk426@gmail.com>
2022-05-24 19:25:02 +0200

    models register

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Deck, Room, Task, User, Mark
+from .models import Deck, Room, Task, User, Mark, Invitation, UserStory
 
 
 # admin.site.register(User)
@@ -7,3 +7,5 @@ admin.site.register(Room)
 admin.site.register(Deck)
 admin.site.register(Task)
 admin.site.register(Mark)
+admin.site.register(Invitation)
+admin.site.register(UserStory)
