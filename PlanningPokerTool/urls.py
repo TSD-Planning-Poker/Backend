55ebe9f1b856bcfff77a65fb191298278b68c43e
krzk426 <krzk426@gmail.com>
2022-06-05 21:43:13 +0200

    static files url

--- PlanningPokerTool/urls.py
+++ PlanningPokerTool/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path, re_path
 from django.contrib import admin
 from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
-
+from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 import PlanningPokerTool.settings as settings
@@ -32,7 +32,8 @@ urlpatterns = [
     path('base/', include('base.urls')),
     path('api/', include('api.urls')),
     path('auth/', include('authentication.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 # Add Swagger if django is in dev mode
 if settings.DEBUG:
